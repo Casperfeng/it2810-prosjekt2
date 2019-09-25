08f3db0d84e6cb7122866cdb4012c42bdf267aef
Ellen Zhang Chang <ellenzc@stud.ntnu.no>
2019-09-25 19:51:01 +0200

    Style/header

--- src/components/Header/Menu/SideNav/SideNav.js
+++ src/components/Header/Menu/SideNav/SideNav.js
@@ -4,18 +4,19 @@ import './SideNav.css';
 export default function SideNav(props) {
   return (
     <div className='sidenav'>
-      <div className='menu-item'>Bilder</div>
-      <p className='menu-option' onClick={() => props.onSvgIndexChanged(0)}>Høst</p>
-      <p className='menu-option' onClick={() => props.onSvgIndexChanged(1)} > Sommer</p>
-      <p className='menu-option' onClick={() => props.onSvgIndexChanged(2)} > Vinter</p>
-      <div className='menu-item'>Lyd</div>
-      <p className='menu-option' onClick={() => props.onAudioIndexChanged(0)} >Japansk</p>
-      <p className='menu-option' onClick={() => props.onAudioIndexChanged(1)}>Latter</p>
-      <p className='menu-option' onClick={() => props.onAudioIndexChanged(2)}>Musikk</p>
-      <div className='menu-item'>Tekst</div>
-      <p className='menu-option' onClick={() => props.onPoemIndexChanged(0)} >Katt</p>
-      <p className='menu-option' onClick={() => props.onPoemIndexChanged(1)}>Hund</p>
-      <p className='menu-option' onClick={() => props.onPoemIndexChanged(2)}>Kjærlighet</p>
+      <div className='category'>Image</div>
+      <p className={`option ${props.svgIndex === 0 ? 'option-active' : ''}`} onClick={() => props.onSvgIndexChanged(0)}>Autumn</p>
+      <p className={`option ${props.svgIndex === 1 ? 'option-active' : ''}`} onClick={() => props.onSvgIndexChanged(1)} >Summer</p>
+      <p className={`option ${props.svgIndex === 2 ? 'option-active' : ''}`} onClick={() => props.onSvgIndexChanged(2)} >Winter</p>
+      <div className='category'>Sound</div>
+      <p className={`option ${props.audioIndex === 0 ? 'option-active' : ''}`} onClick={() => props.onAudioIndexChanged(0)} >Japanese</p>
+      <p className={`option ${props.audioIndex === 1 ? 'option-active' : ''}`} onClick={() => props.onAudioIndexChanged(1)}>Laughter</p>
+      <p className={`option ${props.audioIndex === 2 ? 'option-active' : ''}`} onClick={() => props.onAudioIndexChanged(2)}>Music</p>
+      <div className='category'>Poem</div>
+      <p className={`option ${props.poemIndex === 0 ? 'option-active' : ''}`} onClick={() => props.onPoemIndexChanged(0)} >Cat</p>
+      <p className={`option ${props.poemIndex === 1 ? 'option-active' : ''}`} onClick={() => props.onPoemIndexChanged(1)}>Dog</p>
+      <p className={`option ${props.poemIndex === 2 ? 'option-active' : ''}`} onClick={() => props.onPoemIndexChanged(2)}>Love</p>
+      <div className="empty-box"></div>
     </div>
   )
 }
