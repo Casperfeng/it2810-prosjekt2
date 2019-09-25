71a53cb80584098eeb8c070ed13073693c981b34
Casper Feng <casp.feng@gmail.com>
2019-09-25 11:33:19 +0200

    fix sidebar. Close issue #6

--- src/components/Header/Menu/Menu.js
+++ src/components/Header/Menu/Menu.js
@@ -6,9 +6,19 @@ import './Menu.css';
 export default function Menu(props) {
   const [clicked, setClicked] = useState(false);
   return (
-    <div className='menu' onClick={() => setClicked(!clicked)}>
-      <BurgerMenu clicked={clicked} />
-      {clicked && <SideNav />}
-    </div>
+    <>
+      <div className='menu' onClick={() => setClicked(!clicked)}>
+        <BurgerMenu clicked={clicked} />
+      </div>
+      {
+        clicked && <SideNav
+          svgIndex={props.svgIndex}
+          poemIndex={props.poemIndex}
+          audioIndex={props.audioIndex}
+          onSvgIndexChanged={props.onSvgIndexChanged}
+          onPoemIndexChanged={props.onPoemIndexChanged}
+          onAudioIndexChanged={props.onAudioIndexChanged} />
+      }
+    </>
   )
 }
--- src/components/Header/Menu/SideNav/SideNav.js
+++ src/components/Header/Menu/SideNav/SideNav.js
@@ -5,6 +5,18 @@ export default function SideNav(props) {
   return (
     <div className='sidenav'>
 
+      <div className='menu-item'>Bilder</div>
+      <p className='menu-option' onClick={() => props.onSvgIndexChanged(0)}>Høst</p>
+      <p className='menu-option' onClick={() => props.onSvgIndexChanged(1)} > Sommer</p>
+      <p className='menu-option' onClick={() => props.onSvgIndexChanged(2)} > Vinter</p>
+      <div className='menu-item'>Lyd</div>
+      <p className='menu-option' onClick={() => props.onAudioIndexChanged(0)} >Japansk</p>
+      <p className='menu-option' onClick={() => props.onAudioIndexChanged(1)}>Latter</p>
+      <p className='menu-option' onClick={() => props.onAudioIndexChanged(2)}>Musikk</p>
+      <div className='menu-item'>Tekst</div>
+      <p className='menu-option' onClick={() => props.onPoemIndexChanged(0)} >Katt</p>
+      <p className='menu-option' onClick={() => props.onPoemIndexChanged(1)}>Hund</p>
+      <p className='menu-option' onClick={() => props.onPoemIndexChanged(2)}>Kjærlighet</p>
     </div>
   )
 }
