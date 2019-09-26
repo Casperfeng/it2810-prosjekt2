1c6758c14e52ddbd4104f50910cc3490a10a7e9d
EivindYN <eivindyunilsen@hotmail.com>
2019-09-26 13:05:56 +0200

    Clean and reformat all code. Close issue #15

--- src/components/Header/Menu/HistoryMenu/HistoryMenu.js
+++ src/components/Header/Menu/HistoryMenu/HistoryMenu.js
@@ -1,28 +1,33 @@
-import React from 'react'
+import React from 'react';
 import './HistoryMenu.css';
-class HistoryMenu extends React.Component { //TODO: Useless constructor?
-  constructor(props) {
-    super(props);
-  }
+class HistoryMenu extends React.Component {
   undoColor() {
-    return this.props.historyIndex > 1 ? "white" : "#444444";
+    return this.props.historyIndex > 1 ? 'white' : '#444444';
   }
 
   redoColor() {
-    return this.props.historyIndex < this.props.historySize ? "white" : "#444444";
+    return this.props.historyIndex < this.props.historySize
+      ? 'white'
+      : '#444444';
   }
 
   render() {
-    return <div className="historymenu">
-      <svg className="historyBtn" onClick={this.props.handleUndo}>
-        <polygon fill={this.undoColor()}
-          points="25,15 25,22 41,22 41,38 25,38 25,45 5,30" />
-      </svg>
-      <svg className="historyBtn" onClick={this.props.handleRedo}>
-        <polygon fill={this.redoColor()}
-          points="20,15 20,22 4,22 4,38 20,38 20,45 40,30" />
-      </svg>
-    </div>
+    return (
+      <div className='historymenu'>
+        <svg className='historyBtn' onClick={this.props.handleUndo}>
+          <polygon
+            fill={this.undoColor()}
+            points='25,15 25,22 41,22 41,38 25,38 25,45 5,30'
+          />
+        </svg>
+        <svg className='historyBtn' onClick={this.props.handleRedo}>
+          <polygon
+            fill={this.redoColor()}
+            points='20,15 20,22 4,22 4,38 20,38 20,45 40,30'
+          />
+        </svg>
+      </div>
+    );
   }
 }
 
--- src/components/Header/Menu/SideNav/SideNav.test.js
+++ src/components/Header/Menu/SideNav/SideNav.test.js
@@ -3,9 +3,6 @@ import SideNav from './SideNav';
 import renderer from 'react-test-renderer';
 
 it('renders SideNav correctly', () => {
-  const tree = renderer
-    .create(<SideNav />)
-    .toJSON();
+  const tree = renderer.create(<SideNav />).toJSON();
   expect(tree).toMatchSnapshot();
 });
-
